c527aef35a52772d205ebeee7633ca2e1a3e7a33
MomchilDeus <momchil.kolev@protonmail.com>
2018-03-12 21:49:32 +0200

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://kolev:123StrongPassword!!@ds261088.mlab.com:61088/storybooks-dev',
+    googleClientID: '590351795170-qdhe4umumm4bmg47vq1l7i8cdvj9r2gg.apps.googleusercontent.com',
+    googleClientSecret: 'sMlcp0ZYKdzdUsVlE3b_W89r'
+}
